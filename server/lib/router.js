57feb3bb07f701081e658ce1a677c35fb8d77dd5
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-07-25 01:40:34 -0400

    new routes

---
+++ server/lib/router.js
@@ -0,0 +1,23 @@
+module.exports = (app) => {
+    const path = require("path")
+
+    app.get("/", (req, res)=> {
+        res.sendFile(path.resolve(__dirname + '/../public/index.html'))
+    })
+
+    app.get("/mouse-control", (req, res)=> {
+        res.sendFile(path.resolve(__dirname + '/../public/mouse-control.html'))
+    })
+
+    app.get("/turret-control", (req, res)=> {
+        res.sendFile(path.resolve(__dirname + '/../public/turret-control.html'))
+    })
+
+    app.get("/gyro-control", (req, res)=> {
+        res.sendFile(path.resolve(__dirname + '/../public/gyro-control.html'))
+    })
+    
+    app.get("/gyro-mock", (req, res)=> {
+        res.sendFile(path.resolve(__dirname + '/../public/gyro-mock.html'))
+    })
+}
