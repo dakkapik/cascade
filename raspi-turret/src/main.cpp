8daf2bc280ee480afd20ffd4368adfacbb6ab2a4
Gabriel Igliozzi <gabeiglio@Gabriels-MacBook-Pro.local>
2022-08-03 00:53:14 -0400

    Algorithm to parse gyro semantics

--- raspi-turret/src/main.cpp
+++ raspi-turret/src/main.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
-#include <wiringPi.h>
-#include <stdio.h>
-#include <softPwm.h>
 #include <thread>
+#include <string>
+#include <vector>
+
+#include <wiringPi.h>
+#include <softPwm.h>
 
 #define SERVO_MIN_MS 0   // Define the pulse duration for minimum angle of servo
 #define SERVO_MAX_MS 28  // Define the pulse duration for maximum angle of servo
 
 #define servo_pitch 0
 #define servo_yaw   7       // Define the GPIO number connected to servo
+
+struct coordinate {
+    double angx, angy, angz; //angular velocity values
+    double accx, accy, accz; // linear acceleration values
+};
+
+coordinate parseDataInput(std::string& input) {
+
+    std::vector<double> values;
+
+    int begin = 0;
+    int last  = 0;
+
+    while (last <= input.length()) {
+        if (input[last] == ' ' || input[last] == '\0') {
+            values.push_back(std::stof(input.substr(begin, begin - last - 1)));
+            begin = ++last;
+        }
+        else {
+            last++;
+        }
+    }
+
+    if (values.size() != 6) {
+        std::cerr << "[ERROR] Number of arguments is different than 6" << std::endl;
+    }
+
+    coordinate result =  {values[0], values[1], values[2], values[3], values[4], values[5]};
+    return result;
+}
 
 void servoInit(int pin) {         // Initialization function for servo PMW pin
      softPwmCreate(pin, 0, 200);  // Using pin 0 (GPIO 17), position 0 degrees, and pmwRange of 200 (20ms)
@@ -22,34 +54,13 @@ void servoWriteMS(int pin, int ms) {     // Specify the unit for pulse (5-25ms) 
      softPwmWrite(pin, ms);               // This function makes the servo move
  }
 
-std::tuple<int, int> parse_position(std::string input) {
-    std::string x;
-    int last_input = 0;
-    for (int i = 0; i < input.length(); i++) {
-        if (input[i] != " ") {
-            x.append(input[i]);
-        } else { last_input = i; break; }
-    }
-
-    std::string y;
-    for (int i = last_input + 1; i < input.length(); i++) {
-        if (input[i] != " ") {
-            y.append(input[i]);
-        } else { break; }
-    }
-
-    return std::make_tuple(stoi(x), stoi(y));
-}
-
 void set_instruction(std::string* adrs) {
      while (true)
          std::cin >> *adrs;
  }
 
- int main(void) {
-     float servoAngle = 24.0f;
+ int main(void) {     
      std::string instruction;
-     bool isLeft = false;
 
      wiringPiSetup();            // Setup of GPIO pins to wiringPi pin layout
      servoInit(servoPin);        // Initialize PMW pin of servo, in this case 0 (GPIO 17)
@@ -57,7 +68,9 @@ void set_instruction(std::string* adrs) {
      std::thread some(set_instruction, &instruction);
 
     while (true) {
-        std::tupple<int, int> pos = parse_position(instruction);
+        coordinate result = parseDataInput(instruction);
+
+        //Need to figure out a way to translate the coordinates into actual movements of the servos
         servoWriteMS(servo_pitch, pos(1));
         servoWriteMS(servo_yaw, pos(2));     
     }
