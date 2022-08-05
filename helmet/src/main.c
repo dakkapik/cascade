3646028f887670b04d67d2dbb9deae62efee1941
Gabriel Igliozzi <gaboiglio@gmail.com>
2022-08-05 17:46:04 -0400

    Fix samples rates

--- helmet/src/main.c
+++ helmet/src/main.c
@@ -6,10 +6,24 @@ int main(int argc, char* argv[]) {
     mpu m;
     mpu_init(&m, 0x68);
 
-    mpu_set_sample_rate(&m, 0x07);
+    mpu_set_sample_rate(&m, 0x08);
+    listen_clock_rate(&m);
     printf("$ %f\n", m.sample_rate);
 
-    //Runtime of the chip
+    int cycles = 0;
+    while (cycles < 1000) { 
+        listen_gyro_coordinate(&m);
+        listen_accl_coordinate(&m);
+        printf("\r%f %f %f %f %f %f", m.gyro.x, m.gyro.y, m.gyro.z,  m.accl.x, m.accl.y, m.accl.z);
+        fflush(stdout);
+        delay(100);
+        cycles++;
+    }
+
+    mpu_set_sample_rate(&m, 0x04);
+    listen_clock_rate(&m);
+    printf("$ %f\n", m.sample_rate);
+
     while (1) {
         listen_gyro_coordinate(&m);
         listen_accl_coordinate(&m);
