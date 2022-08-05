921d6d9f4cf7488fc2371593020b113af33e52e1
Gabriel Igliozzi <gaboiglio@gmail.com>
2022-08-05 17:56:16 -0400

    test sample output

--- helmet/src/main.c
+++ helmet/src/main.c
@@ -11,12 +11,11 @@ int main(int argc, char* argv[]) {
     printf("$ %f\n", m.sample_rate);
 
     int cycles = 0;
-    while (cycles < 1000) { 
+    while (cycles < 500) { 
         listen_gyro_coordinate(&m);
         listen_accl_coordinate(&m);
         printf("\r%f %f %f %f %f %f", m.gyro.x, m.gyro.y, m.gyro.z,  m.accl.x, m.accl.y, m.accl.z);
         fflush(stdout);
-        delay(100);
         cycles++;
     }
 
@@ -29,7 +28,6 @@ int main(int argc, char* argv[]) {
         listen_accl_coordinate(&m);
         printf("\r%f %f %f %f %f %f", m.gyro.x, m.gyro.y, m.gyro.z,  m.accl.x, m.accl.y, m.accl.z);
         fflush(stdout);
-        delay(100);
     }
 
 }
