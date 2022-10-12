c3eb5357c85c01152e6dfdf3380a53aa144dfb2c
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-10-12 00:00:04 -0400

    Update test.js

--- helmet/test.js
+++ helmet/test.js
@@ -1,16 +1,8 @@
 'use strict'
 const c_process = require("./c_process")
 
-try{
-    const child = c_process.spawnPyBuild()
+const child = c_process.spawnPyBuild()
 
-    child.stdout.on('data', (data) =>{
-        console.log(data)
-    })
-
-    // child.stdout.pipe(process.stdout)
-
-} catch (err) {
-    console.error("ERROR: ", err)
-}
-
+child.stdout.on('data', (data) =>{
+    console.log(data)
+})
