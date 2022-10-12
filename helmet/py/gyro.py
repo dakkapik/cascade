aed8d904a7a587f7b9cbe3463454cbd383e031c6
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-10-12 00:55:01 -0400

    Update gyro.py

--- helmet/py/gyro.py
+++ helmet/py/gyro.py
@@ -71,6 +71,7 @@ while True:
 	Gy = gyro_y/131.0
 	Gz = gyro_z/131.0
 	
-	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+	print (Gx," ",Gy," ",Gz," ",Ax," ",Ay," ",Az)
+	# gx gy gz - ax ay az	
 	# print ("TEST") 	
 	sleep(0.1)
