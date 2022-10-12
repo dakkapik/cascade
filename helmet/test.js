3964ecb41c17f3e428d28dfa423c222f08d25b0a
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-10-12 00:44:35 -0400

    Update test.js

--- helmet/test.js
+++ helmet/test.js
@@ -3,13 +3,13 @@ const { spawn } = require("child_process")
 const path = require("path")
 const c_process = require("./c_process")
 
-// const child = c_process.spawnPyBuild()
+const child = c_process.spawnPyBuild()
 
-// child.stdout.pipe(process.stdout)
+child.stdout.pipe(process.stdout)
 
-const child = spawn('python', ['test.py'], {
-    stdio: ['ignore', 'pipe', 'ignore'],
-    cwd: path.resolve(path.join(__dirname,'py'))
-})
+// const child = spawn('python', ['test.py'], {
+//     stdio: ['ignore', 'pipe', 'ignore'],
+//     cwd: path.resolve(path.join(__dirname,'py'))
+// })
 
 child.stdout.pipe(process.stdout)
