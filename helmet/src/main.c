ae17f9287f63b63a0196de33c5f7d6ca5393bcc0
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-10-12 23:01:15 -0400

    testing without sample rate change

--- helmet/src/main.c
+++ helmet/src/main.c
@@ -10,26 +10,26 @@ int main(int argc, char* argv[]) {
     mpu m;
     mpu_init(&m, GYRO_LOCATION_I2C);
 
-    mpu_set_sample_rate_divisor(&m, FAST_SAMPLE_RATE);
-    listen_clock_rate(&m);
-    printf("$ %f\n", m.sample_rate);
+    // mpu_set_sample_rate_divisor(&m, FAST_SAMPLE_RATE);
+    // listen_clock_rate(&m);
+    // printf("$ %f\n", m.sample_rate);
 
-    int cycles = 0;
-    while (cycles < 500) { 
-        listen_gyro_coordinate(&m);
-        listen_accl_coordinate(&m);
-        printf("%f %f %f %f %f %f %f", m.gyro.x, m.gyro.y, m.gyro.z,  m.accl.x, m.accl.y, m.accl.z, m.sample_rate);
-        fflush(stdout);
-        cycles++;
-    }
+    // int cycles = 0;
+    // while (cycles < 500) { 
+    //     listen_gyro_coordinate(&m);
+    //     listen_accl_coordinate(&m);
+    //     printf("%f %f %f %f %f %f %f", m.gyro.x, m.gyro.y, m.gyro.z,  m.accl.x, m.accl.y, m.accl.z, m.sample_rate);
+    //     fflush(stdout);
+    //     cycles++;
+    // }
 
     mpu_set_sample_rate_divisor(&m, NORMAL_SAMPLE_RATE);
-    listen_clock_rate(&m);
-    printf("$ %f\n", m.sample_rate);
+    // listen_clock_rate(&m);
+    // printf("$ %f\n", m.sample_rate);
 
     while (1) {
         listen_gyro_coordinate(&m);
-        listen_accl_coordinate(&m);
+        // listen_accl_coordinate(&m);
         // printf("%f %f %f %f %f %f %f", m.gyro.x, m.gyro.y, m.gyro.z,  m.accl.x, m.accl.y, m.accl.z, m.sample_rate);
         printf("%f %f %f %f %f %f %f", m.gyro.x, m.gyro.y, m.gyro.z,"%f\n");
         fflush(stdout);
